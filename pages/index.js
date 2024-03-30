2fec0f65fbe2b23fe0b8e901c99e96d4be5d3d23
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2024-03-30 18:42:39 -0400

    added local parsing

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Index from '../components/Index';
 
 export default function Home() {
   const [userIn, setUserIn] = useState("");
+  const [sentences, setSentences] = useState([]);
+  const [facts, setFacts] = useState([]);
 
   const setUserContent = () => {
     setUserIn(document.getElementById('userIn').value);
-    console.log(document.getElementById('userIn').value);
   };
+
+  useEffect(() => {
+    console.log(userIn);
+    const sentences = [];
+    const words = userIn.split(".");
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i + 1; j <= words.length; j++) {
+            sentences.push(words.slice(i, j).join("."));
+        }
+    }
+    setSentences(sentences);
+  }, [userIn]);
+
+  useEffect(() => {
+    console.log(sentences);
+    // code on calling Gemini
+    
+
+  }, [sentences]);
 
   return (
     <>
