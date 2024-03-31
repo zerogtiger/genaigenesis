35b3fd840787fbeea0391d13d1efaefb7daa2c2a
steph <saliu1229@gmail.com>
2024-03-30 21:33:17 -0400

    api calls to gemini thru us vpn

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Index from '../components/Index';
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 
 export default function Home() {
   const [userIn, setUserIn] = useState("");
   const [sentences, setSentences] = useState([]);
   const [facts, setFacts] = useState([]);
+
+  
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI('AIzaSyBPzz2sfZS2Zh7onhJdq-eJDwuNAeF0N4Q');
+
+// ...
+
+// For text-only input, use the gemini-pro model
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+// ...
 
   const setUserContent = () => {
     setUserIn(document.getElementById('userIn').value);
@@ -25,9 +38,16 @@ export default function Home() {
   useEffect(() => {
     console.log(sentences);
     // code on calling Gemini
-    
+    const prompt = "Write a story about a magic back  pack."
+
+    model.generateContent(prompt).then((value) => {console.log(value)})
+    // const response = await result.response;
+    // const text = response.text();
+    // console.log(text);
 
   }, [sentences]);
+
+
 
   return (
     <>
